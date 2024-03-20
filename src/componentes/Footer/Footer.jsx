1354e58324fa9812f6c0a9536b186a79cabcce5b
facu <facuramirez937@gmail.com>
2024-03-20 00:12:01 -0300

    modificaciones extras de nav e imagenes

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
+import { FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa'; // Importa los iconos
 
 const Footer = () => {
   return (
-    <footer className='footer'>
+    <footer>
+      {/* Contenido del footer */}
       <div>
+        <a href="https://facebook.com/tu_perfil" target="_blank" rel="noopener noreferrer">
+          <FaFacebook /> Facebook
+        </a>
+        <a href="https://instagram.com/tu_perfil" target="_blank" rel="noopener noreferrer">
+          <FaInstagram /> Instagram
+        </a>
         <a href="https://wa.me/tunumero" target="_blank" rel="noopener noreferrer">
-          Contacto por WhatsApp
+          <FaWhatsapp /> WhatsApp
         </a>
-        <a href="https://tusocial.com/tuperfil" target="_blank" rel="noopener noreferrer">
-          Sígueme en facebook
-        </a>
-        <a href="https://tusocial.com/tuperfil" target="_blank" rel="noopener noreferrer">
-          Sígueme en Instagram
-        </a>
-
       </div>
     </footer>
   );
 };
 
 export default Footer;
+
